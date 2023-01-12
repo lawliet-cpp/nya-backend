49896445b4f91a383343b48aabdca6bb4f3de186
L Lawliet <60004748+lawliet-cpp@users.noreply.github.com>
2023-01-12 15:30:04 +0100

    Update connect.go

--- database/connect.go
+++ database/connect.go
@@ -1,14 +1,14 @@
 package database
 
 import (
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func Connect() {
-	db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	db, err := gorm.Open(fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", "containers-us-west-56.railway.app", "postgres","GldyzYjiQPO2IA4rp6FN" , "railway", "7332", &gorm.Config{})
 
 	if err != nil {
 		panic("failed to connect database")
